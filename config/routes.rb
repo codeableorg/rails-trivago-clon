f4268e5bb542811aa04c6a7a8810b413dc02ca6f
Cesar Cachay <cesarcachay1996@gmail.com>
2019-04-29 17:25:18 -0500

    Add controllers of resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'bookings/index'
+  get 'bookings/show'
+  get 'bookings/new'
+  get 'bookings/edit'
+  get 'rooms/index'
+  get 'rooms/show'
+  get 'rooms/new'
+  get 'rooms/edit'
+  get 'hotels/index'
+  get 'hotels/show'
+  get 'hotels/new'
+  get 'hotels/edit'
   devise_for :users
   root to: 'home#index'
 end
