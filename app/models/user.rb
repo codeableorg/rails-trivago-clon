e758672932021e8b5a58869e426d9cfd475b9c10
byteconde <iamdeyvi@gmail.com>
2019-04-30 15:57:39 -0500

    Lines commented, issue pending

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,15 @@
 class User < ApplicationRecord
-  has_secure_password
-  has_secure_token
+            # has_secure_password
+            # has_secure_token
 
-  def invalidate_token
-    update(token: nil)
-  end
+            # def invalidate_token
+            #   update(token: nil)
+            # end
 
-  def self.valid_login?(email, password)
-    user = find_by(email: email)
-    user if user && user.authenticate(password)
-  end
+            # def self.valid_login?(email, password)
+            #   user = find_by(email: email)
+            #   user if user && user.authenticate(password)
+            # end
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
