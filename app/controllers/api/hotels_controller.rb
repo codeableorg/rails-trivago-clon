c12862f7a4c48d6dbaa7c4041e7cbbc01b0403ed
byteconde <iamdeyvi@gmail.com>
2019-04-30 10:48:08 -0500

    Hotel controller init and CRUD done

---
+++ app/controllers/api/hotels_controller.rb
@@ -0,0 +1,46 @@
+class Admin::HotelsController < ApplicationController
+  def index
+    render json: Hotel.all
+  end
+
+  def show
+    render json: Hotel.find(params[:id])
+  end
+
+  def create
+    hotel = Hotel.new(hotel_params)
+    if hotel.save
+      render json: hotel
+    else
+      render json: { status: 'ERROR', 
+                     message: 'Hotel not saved' }, 
+                     status: status: :unprocessable_entity
+    end
+
+  def destroy
+    hotel = Hotel.find(params[:id])
+    hotel.destroy
+      render json: { status: 'SUCCESS',
+                     message: 'Hotel deleted', },
+                     status: :ok
+  end
+
+  def update
+    hotel = Hotel.find(params[:id])
+    if hotel.update_attributes(hotel_params)
+      render json: { status: 'SUCCESS',
+                     message: 'Updated article',} , 
+                     status: :ok
+    else
+      render json: { status: 'ERROR',
+                     message: 'Hotel not updated',} , 
+                     status: :unprocessable_entity
+  end
+end
+
+  
+  private
+  def hotel_params
+    params.permit(:name,:email,:city,:country,:address)
+  end
+end
