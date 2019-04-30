6f18c18dd6ff6c333b6304a520a32ae299a47fe8
Cristian Granda <cristian.granda.pastor@gmail.com>
2019-04-30 08:49:42 -0500

    Add controller for hotels and rooms in admin (#13)
    * Add controller for hotels and rooms in admin
    
    * Add basic views to admin-hotels and admin-rooms
    
    * Add all views to admin-hotel controller
    
    * Add views for admin-rooms

--- app/controllers/admin/rooms_controller.rb
+++ app/controllers/admin/rooms_controller.rb
@@ -1,13 +1,50 @@
 class Admin::RoomsController < ApplicationController
+  before_action :set_rooms, only: [:show, :edit, :update, :destroy]
+
   def index
+    @rooms = Room.all
+  end
+
+  def new
+    @room = Room.new
   end
 
   def show
   end
-
-  def new
+  
+  def edit
   end
 
-  def edit
+  def create
+    @room = Room.create(room_params)
+
+    if @room.save 
+      redirect_to admin_room_path(@room), notice: 'Room already created!'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @room.update(room_params)
+      redirect_to admin_room_path(@room), notice: 'Room already updated!'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @room.destroy
+    redirect_to admin_rooms_path, notice: "Room already deleted!"
+  end
+
+  private
+
+  def set_rooms
+    @room = Room.find(params[:id])
+  end
+
+  def room_params
+    params.require(:room).permit(:name, :amount_of_beds, :price, :hotel_id, :address)
   end
 end
