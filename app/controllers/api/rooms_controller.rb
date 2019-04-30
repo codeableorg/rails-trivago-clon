2cb9965d0203aeff40630758c5c46e9d23fe0be0
byteconde <iamdeyvi@gmail.com>
2019-04-30 11:59:12 -0500

    Room CRUD for API

--- app/controllers/api/rooms_controller.rb
+++ app/controllers/api/rooms_controller.rb
@@ -1,13 +1,47 @@
-class Admin::RoomsController < ApplicationController
-  def index
-  end
+module Api
+  class Admin::RoomsController < ApplicationController
+    def index
+      render json: Room.all
+    end
 
-  def show
-  end
+    def show
+      render json: Room.find(params[:id])
+    end
 
-  def new
-  end
+    def create
+      room = Room.new(room_params)
+      if room.save
+        render json: room
+      else
+        render json: { status: 'ERROR', 
+                      message: 'Room not saved' }, 
+                      status: status: :unprocessable_entity
+    end
 
-  def edit
+    def destroy
+      room = Room.find(params[:id])
+      room.destroy
+        render json: { status: 'SUCCESS',
+                      message: 'Room deleted', },
+                      status: :ok
+    end
+
+    def update
+      room = room.find(params[:id])
+      if room.update_attributes(room_params)
+        render json: { status: 'SUCCESS',
+                      message: 'Updated Room',} , 
+                      status: :ok
+      else
+        render json: { status: 'ERROR',
+                      message: 'Room not updated',} , 
+                      status: :unprocessable_entity
+      end
+    end
+
+    private
+    def room_params
+      params.permit(:name,:amount_of_beds,:price,:hotel_id)
+    end
   end
 end
