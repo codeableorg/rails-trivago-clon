9cb393ad591a365565c03a6bd8a7b3da29f3808c
cristianbgp <cristian.granda.pastor@gmail.com>
2019-04-29 19:11:19 -0500

    Add controller for hotels and rooms in admin

--- app/controllers/admin/hotels_controller.rb
+++ app/controllers/admin/hotels_controller.rb
@@ -1,13 +1,51 @@
 class Admin::HotelsController < ApplicationController
+  before_action :set_hotels, only: [:show, :edit, :update, :destroy]
+
   def index
+    @hotels = Hotel.all
+  end
+
+  def new
+    @hotel = Hotel.new
   end
 
   def show
   end
-
-  def new
+  
+  def edit
   end
 
-  def edit
+  def create
+    @hotel = Hotel.create(set_hotels)
+    @hotel = current_user.hotels.create(set_hotels)
+
+    if @hotel.save 
+      redirect_to hotels_path(@hotel), notice: 'Hotel already created!'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @hotel.update(hotel_params)
+      redirect_to hotels_path(@hotel), notice: 'Hotel already updated!'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @hotel.destroy
+    redirect_to hotels_path, notice: "Hotel already deleted!"
+  end
+
+  private
+
+  def set_hotels
+    @hotel = Hotel.find(params[:id])
+  end
+
+  def hotel_params
+    params.require(:hotel).permit(:name, :email, :city, :country, :address)
   end
 end
--- app/controllers/admin/rooms_controller.rb
+++ app/controllers/admin/rooms_controller.rb
@@ -1,13 +1,51 @@
 class Admin::RoomsController < ApplicationController
+  before_action :set_rooms, only: [:show, :edit, :update, :destroy]
+
   def index
+    @rooms = Room.all
+  end
+
+  def new
+    @room = Room.new
   end
 
   def show
   end
-
-  def new
+  
+  def edit
   end
 
-  def edit
+  def create
+    @room = Room.create(set_rooms)
+    @room = current_user.rooms.create(set_rooms)
+
+    if @room.save 
+      redirect_to rooms_path(@room), notice: 'Room already created!'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @room.update(room_params)
+      redirect_to rooms_path(@room), notice: 'Room already updated!'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @room.destroy
+    redirect_to rooms_path, notice: "Room already deleted!"
+  end
+
+  private
+
+  def set_rooms
+    @room = Room.find(params[:id])
+  end
+
+  def room_params
+    params.require(:room).permit(:name, :amount_of_beds, :price, :hotel_id, :address)
   end
 end
