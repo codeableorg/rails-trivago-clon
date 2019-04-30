f2452e1e4611d5c871b42c97f39bed341d7435f4
Ricardo Yrupailla Meza <ry.yrupailla@gmail.com>
2019-04-30 17:56:33 -0500

    Regular user (#33)
    * Add controller for hotels
    
    * List hotels
    
    * Search hotel by name
    
    * Add link for show hotel
    
    * Add search for rooms
    
    * Add search room by price
    
    * Add show method for rooms
    
    * Add conflict detection for booking
    
    * Fix merge conflicts
    
    * Fix merge conflicts

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,6 +1,27 @@
 class RoomsController < ApplicationController
 
-  def show
+  def show    
     @room = Room.find(params[:id])
+    @error = ''
+
+    if params[:min_date].present? && params[:max_date].present?
+
+      @bookings = @room.bookings
+      @conflict_ids = @bookings.where(
+        [
+          '(start_date <= :min_date AND end_date >= :max_date)',
+          '(start_date >= :min_date AND start_date <= :max_date)',
+          '(end_date >= :min_date AND end_date <= :max_date)'
+        ].join(' OR'),
+        {
+          min_date: params[:min_date],
+          max_date: params[:max_date]
+        }  
+      ).ids
+
+    else
+      @bookings = @room.bookings
+    end
+    
   end
 end
