21b0de6cb8f084b0eca281c76ca62da96a7a55e9
byteconde <iamdeyvi@gmail.com>
2019-04-30 11:58:17 -0500

    Booking CRUD API

--- app/controllers/api/bookings_controller.rb
+++ app/controllers/api/bookings_controller.rb
@@ -1,13 +1,47 @@
-class Admin::BookingsController < ApplicationController
-  def index
-  end
+module Api
+  class Admin::BookingsController < ApplicationController
+    def index
+      render json: Booking.all
+    end
 
-  def show
-  end
+    def show
+      render json: Booking.find(params[:id])
+    end
 
-  def new
-  end
+    def create
+      booking = Booking.new(booking_params)
+      if booking.save
+        render json: booking
+      else
+        render json: { status: 'ERROR', 
+                      message: 'Booking not saved' }, 
+                      status: status: :unprocessable_entity
+    end
 
-  def edit
+    def destroy
+      booking = Booking.find(params[:id])
+      booking.destroy
+        render json: { status: 'SUCCESS',
+                      message: 'Booking deleted', },
+                      status: :ok
+    end
+
+    def update
+      booking = booking.find(params[:id])
+      if booking.update_attributes(booking_params)
+        render json: { status: 'SUCCESS',
+                      message: 'Updated Booking',} , 
+                      status: :ok
+      else
+        render json: { status: 'ERROR',
+                      message: 'Booking not updated',} , 
+                      status: :unprocessable_entity
+      end
+    end
+
+    private
+    def booking_params
+      params.permit(:start_date,:end_date,:paid_price,:user_id,:room_id)
+    end
   end
 end
