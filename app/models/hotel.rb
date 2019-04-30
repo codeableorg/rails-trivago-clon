f0db94f432fc5af124971740542d5740dc3f147c
cristianbgp <cristian.granda.pastor@gmail.com>
2019-04-30 12:43:55 -0500

    Add delete_all to the relation between hotel and rooms

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,5 +1,5 @@
 class Hotel < ApplicationRecord
-  has_many :rooms
+  has_many :rooms, dependent: :delete_all
   has_many :promotions, as: :promotionable
   has_many :bookings, through: :rooms
   has_one_attached :cover
