d8735fdd645a7af293cecd6f6e1ce9ec67b1d42d
cristianbgp <cristian.granda.pastor@gmail.com>
2019-05-01 18:14:37 -0500

    nav for regular or admin user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  # root to: 'home#index'
+  root to: 'home#index'
   namespace :admin do
+    root 'home#index'
     resources :hotels
     resources :rooms
     resources :bookings
