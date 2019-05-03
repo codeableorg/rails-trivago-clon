4cb10a33946b9febff4732dc185e85826062a9e6
cristianbgp <cristian.granda.pastor@gmail.com>
2019-05-03 10:27:11 -0500

    Add validations for hotel

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -5,6 +5,12 @@ class Hotel < ApplicationRecord
   has_many :bookings, through: :rooms
   has_one_attached :cover
 
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }
+  validates :city, presence: true
+  validates :country, presence: true
+  validates :address, presence: true
+
   def notify_users
     BookingMailer.with(user: user, booking: room_id).notify_users.deliver_later
   end
