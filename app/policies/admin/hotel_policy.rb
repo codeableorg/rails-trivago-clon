d549941d1ef6aa006d02d3876f02cf5f4936654c
Ricardo <ry.yrupailla@gmail.com>
2019-05-03 18:37:27 -0500

    Add pundit for admin

---
+++ app/policies/admin/hotel_policy.rb
@@ -0,0 +1,34 @@
+class Admin::HotelPolicy < ApplicationPolicy
+
+  attr_reader :user, :hotel
+
+
+  def index?
+    @user&.admin?
+  end
+
+  def new?
+    @user&.admin?
+  end
+
+  def show?
+    @user&.admin?
+  end 
+
+  def edit?
+    @user&.admin?
+  end
+ 
+  def create?
+    @user&.admin?
+  end
+
+  def update?
+    @user&.admin?
+  end
+ 
+  def destroy?
+    @user&.admin?
+  end
+ 
+end
