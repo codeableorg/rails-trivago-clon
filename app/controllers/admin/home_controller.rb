19df4e490ea193699f0e773b82a46b7a499739a7
cristianbgp <cristian.granda.pastor@gmail.com>
2019-05-03 10:03:31 -0500

    Add queries to most popular and less popular hotels

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -1,4 +1,15 @@
 class Admin::HomeController < ApplicationController
-  def index    
+  def index
+    @popularhotels = Hotel.joins(rooms: :bookings)
+    .group('id')
+    .order('count(bookings.id) DESC')
+
+    @sumpopularhotels = Hotel.joins(rooms: :bookings)
+    .group('id')
+    .order('SUM(bookings.id) DESC')
+
+    @lesspopularhotels = Hotel.joins(rooms: :bookings)
+    .group('id')
+    .order('count(bookings.id) ASC')
   end
 end
