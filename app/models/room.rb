90ff3b3f04a1db080a0c9e5ea66dbf5a12b23807
Cristian Granda <cristian.granda.pastor@gmail.com>
2019-05-03 11:38:09 -0500

    Deregister a Hotel or Room now deletes them promotions (#57)

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,6 @@
 class Room < ApplicationRecord
   belongs_to :hotel
-  has_many :promotions, as: :promotionable
+  has_many :promotions, as: :promotionable, dependent: :destroy
   has_many :bookings, dependent: :destroy
   has_one_attached :cover
 
