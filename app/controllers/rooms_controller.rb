e481b1ec904bd71cd428c7e3768ef5cc1edeb7a3
Ricardo Yrupailla Meza <ry.yrupailla@gmail.com>
2019-05-03 11:37:57 -0500

    Refactor promotions (#58)
    * refactor booking promotions
    
    * Fix current_user always true

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -37,7 +37,7 @@ class RoomsController < ApplicationController
     if params[:min_date].present? && params[:max_date].present?
 
       @room = Room.find(params[:id])
-      @promotions = @room.promotions
+      @hotel = @room.hotel
       @bookings = @room.bookings
 
       @conflict_ids = @bookings.where(
@@ -51,28 +51,15 @@ class RoomsController < ApplicationController
           max_date: params[:max_date]
         }  
       ).ids
-
       
       if @conflict_ids.none?
         
         paid_price = @room.price
-
-        if @promotions.any? 
-
-          @promotions.each do |promotion|
-            if promotion.start_date <= Date.today && promotion.end_date >= Date.today
-              if promotion.discount_type == 'percentage'
-                paid_price = ((1 - promotion.discount_amount.to_f/100)*paid_price).to_i
-              elsif promotion.discount_type == 'fixed'
-                paid_price = paid_price - promotion.discount_amount
-              end
-            end
-          end
-
-        end
+        total_discount = get_discount(@room)
+        paid_price -= total_discount
 
         paid_price = 0 if paid_price < 0
-
+        
         current_user.bookings.create( 
           start_date: params[:min_date], 
           end_date: params[:max_date], 
@@ -83,4 +70,40 @@ class RoomsController < ApplicationController
       redirect_to action: 'show'
     end  
   end
+
+  def get_discount(room)
+      
+    paid_price = room.price
+    hotel = room.hotel
+    discount = 0
+
+    hotel_discount = 0
+    hotel.promotions.each do |promotion|
+      if promotion.applicable?
+        if promotion.discount_type == 'percentage'
+          discount = ((promotion.discount_amount.to_f/100)*paid_price).to_i
+        elsif promotion.discount_type == 'fixed'
+          discount = promotion.discount_amount
+        end
+
+        hotel_discount = discount if discount > hotel_discount
+      end
+    end
+
+    room_discount = 0
+    room.promotions.each do |promotion|
+      if promotion.applicable?
+        if promotion.discount_type == 'percentage'
+          discount = ((promotion.discount_amount.to_f/100)*paid_price).to_i
+        elsif promotion.discount_type == 'fixed'
+          discount = promotion.discount_amount
+        end
+
+        room_discount = discount if discount > room_discount
+      end
+    end
+
+    hotel_discount + room_discount
+  end
+
 end
--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -1,8 +1,12 @@
 class Promotion < ApplicationRecord
-  belongs_to :promotionable, polymorphic: true
-
+  belongs_to :promotionable, polymorphic: true}
   validates :start_date, presence: true
   validates :end_date, presence: true
   validates :discount_type, presence: true
   validates :discount_amount, presence: true
+
+  def applicable?
+    self.start_date <= Date.today && self.end_date >= Date.today
+  end
+
 end
