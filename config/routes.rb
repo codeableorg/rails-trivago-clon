a3444e5b84abb7d532621df52c51fc5e8bb75a9c
Cristian Granda <cristian.granda.pastor@gmail.com>
2019-05-03 23:17:11 -0500

    Book api (#83)
    * Add promotions controller
    
    * Add policies for promotions
    
    * Delete useless folder
    
    * Fix wrong policy assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'home#index'
+
   namespace :admin do
     root 'home#index'
     resources :hotels do
@@ -11,17 +12,20 @@ Rails.application.routes.draw do
     end
     resources :bookings
   end
+
   namespace :api do
     post "/login", to: 'sessions#create' 
-    resources :hotels
+    resources :hotels do
+      resources :promotions, controller: 'promotions_hotels'
+    end
     resources :bookings
     
     resources :rooms do
       member do
         post 'book'
       end
+      resources :promotions, controller: 'promotions_rooms'
     end
- 
   end
   
   resources :hotels do
