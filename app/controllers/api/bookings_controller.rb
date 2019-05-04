d383cfd1c863d41404cdbb2ddd82264e6ffd6d6e
Carlos A <cayala.w@gmail.com>
2019-05-03 19:54:03 -0500

    Tests (#78)
    * Prepare test for API: work in progress
    
    * Create test for API
    
    * Test API controllers
    
    * Update rooms_controller_spec.rb
    
    Deleting commented lines
    
    * Work in progress

--- app/controllers/api/bookings_controller.rb
+++ app/controllers/api/bookings_controller.rb
@@ -1,53 +1,59 @@
-class Api::BookingsController < ApiController
-  before_action :authorization_method
+module Api
+  class BookingsController < ApiController
+    before_action :authorization_method
+    before_action :set_booking, only: [:show, :update, :destroy]
+    def index
+      # authorize Booking
+      render json: Booking.all
+      # authorize @bookings
+    end
 
-  before_action :set_booking, only: [:show, :update, :destroy]
-  def index
-    render json: Booking.all
-  end
+    def show 
+      # authorize @booking
+      render json: @booking
+    end
 
-  def show 
-    render json: @booking
-  end
+    def show 
+      render json: @booking
+    end
 
-  def create
-    booking = Booking.new(booking_params)
-    if booking.save
-      render json: booking
-    else
-      render json: { message: 'Booking not saved' }, 
-                      status: :unprocessable_entity
+    def create
+      booking = Booking.new(booking_params)
+      if booking.save
+        render json: booking
+      else
+        render json: { message: 'Booking not saved' }, 
+                        status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      @booking.destroy
+        render json: { message: 'Booking deleted' },
+                        status: :ok
+    end
+
+    def update
+      if @booking.update_attributes(booking_params)
+        render json: { message: 'Updated Booking'} , 
+                        status: :ok
+      else
+        render json: { message: 'Booking not updated'} , 
+                        status: :unprocessable_entity
+      end
+    end
+
+    private
+    def booking_params
+      params.permit(:start_date,:end_date,:paid_price,:user_id,:room_id)
+    end
+
+    def set_booking
+      @booking = Booking.find(params[:id])
+    end
+
+    def authorization_method
+      authorize Booking
     end
   end
-
-  def destroy
-    @booking.destroy
-      render json: { message: 'Booking deleted' },
-                      status: :ok
-  end
-
-  def update
-    if @booking.update_attributes(booking_params)
-      render json: { message: 'Updated Booking'} , 
-                      status: :ok
-    else
-      render json: { message: 'Booking not updated'} , 
-                      status: :unprocessable_entity
-    end
-  end
-
-  private
-  def booking_params
-    params.permit(:start_date,:end_date,:paid_price,:user_id,:room_id)
-  end
-
-  def set_booking
-    @booking = Booking.find(params[:id])
-  end
-
-  def authorization_method
-    authorize Booking
-  end
-
 end
-
